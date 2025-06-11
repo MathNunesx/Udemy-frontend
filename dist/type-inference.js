6a34fe25a1acc5533207d29f76c858e93269f11b
Matheus Nunes <matheusjobs04@gmail.com>
2025-06-10 22:56:54 -0300

    Readonly

---
+++ dist/type-inference.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let product = 'Fritadeira Elétrica';
+// product = 10 -- error
+exports.default = product;
---
+++ dist/type-tuples.js
@@ -0,0 +1,11 @@
+"use strict";
+let filmArray = [2, 'Vingadores', true];
+let filmTuple = [2, 'Star Wars', false];
+let filmTupleOpcionalPosition = [
+    2,
+    'Star Wars',
+    false,
+];
+const [idArr, titleArr, avaliableArr] = filmArray;
+const [id, title, avaliable] = filmTuple;
+console.log(id);
