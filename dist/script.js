8893f03cdfd0b14bb86e62d21f7f361545d308ed
Matheus Nunes <matheusjobs04@gmail.com>
2025-07-22 22:48:26 -0300

    Array is Array

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-import { bootstrap } from './modules/narrowing/in.js';
+import { bootstrap } from './modules/narrowing/array-isarray.js';
 export { bootstrap };
---
+++ src/modules/narrowing/array-isarray.ts
@@ -0,0 +1,17 @@
+export const bootstrap = (): void => {
+  let arr: number[] | undefined = [10, 20, 30];
+
+  console.log('Typeof de array: ', typeof arr);
+  console.log('Instanceof de array: ', arr instanceof Array);
+  console.log('Array.isArray: ', Array.isArray(arr));
+
+//   type guard
+//   if (Array.isArray(arr)) {
+//     arr?.map((item) => {
+//       console.log(item);
+//     });
+//   }
+  arr?.map((item) => {
+    console.log(item);
+  });
+};
