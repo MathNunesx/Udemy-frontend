ea1147078564d1299552d21d62236c96d064383b
Matheus Nunes <matheusjobs04@gmail.com>
2025-08-07 22:42:45 -0300

    generics em funções

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-import { bootstrap } from './modules/classes/overloading.js';
+import { bootstrap } from './modules/generics/with-functions.js';
 export { bootstrap };
---
+++ src/modules/generics/with-functions.ts
@@ -0,0 +1,37 @@
+export const bootstrap = (): void => {
+  function processAndReturn(value: string | number): string | number {
+    return value;
+  }
+
+  function processAndReturnGeneric<T, U, X>(n: T, s: U, b: X): T | U | X {
+    // return `Texto: ${n} - ${s} ${b}`
+
+    return n;
+  }
+
+  const y = processAndReturnGeneric(7, 'Y', true);
+  console.log(y);
+
+
+//   const x = processAndReturnGeneric('Star Wars');
+//   console.log(x.toUpperCase());
+
+//   const y = processAndReturnGeneric(7);
+//   console.log(y.toFixed(2));
+
+
+// const z = processAndReturnGeneric(true);
+//   console.log(z)
+
+
+  //   const x = processAndReturn('Star Wars');
+
+  //   if (typeof x === 'string') {
+  //     console.log(x.toLocaleUpperCase());
+  //   }
+
+  //   const y = processAndReturn(7);
+  //  if (typeof y === 'number') {
+  //     console.log(y.toFixed(2));
+  //   }
+};
